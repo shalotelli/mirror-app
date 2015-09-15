b0dfed1aaac544cc05591bf57c9f7790ec7b76e7
Sha <sha@responsiveshops.com>
2015-09-15 02:00:22 -0400

    add component directive

---
+++ www/js/directives/addComponent.js
@@ -0,0 +1,49 @@
+(function () {
+  'use strict';
+
+  angular.module('mirror-app.directives').directive('addComponent', addComponentFunc);
+
+  function addComponentFunc ($rootScope, $window, $ionicModal, DashboardComponents, RegisteredComponents) {
+    var addComponentDirective = {
+      restrict: 'E',
+      replace: true,
+      template: '<button class="button button-large button-positive add-component-button" ng-show="inEditMode()" ng-click="openAddComponentModal()">+</button>',
+      controller: addComponentController,
+      link: addComponentLink
+    };
+
+    function addComponentController ($scope, $element, $attrs) {
+      $scope.registeredComponents = RegisteredComponents;
+      $scope.inEditMode = DashboardComponents.inEditMode;
+
+      $scope.openAddComponentModal = openAddComponentModal;
+      $scope.addComponent = addComponent;
+
+      $ionicModal.fromTemplateUrl('templates/directives/addComponentModal.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+      }).then(function(modal) {
+        $scope.modal = modal;
+      });
+
+      function openAddComponentModal () {
+        $scope.modal.show();
+      }
+
+      function addComponent (component) {
+        DashboardComponents.$add({
+          component: component.key
+        }).then(function (ref) {
+          $scope.modal.hide();
+          $rootScope.$broadcast('update-components', ref.key());
+        });
+      }
+    }
+
+    function addComponentLink (scope, element, attrs) {
+      element.css('top', ($window.innerHeight - 75) + 'px');
+    }
+
+    return (addComponentDirective);
+  }
+})();
