628c7a5e1abf35420b54e03499c44ecbad7894aa
Sha <sha@responsiveshops.com>
2015-09-15 01:59:31 -0400

    update shortTime component

--- www/js/components/shortTime/shortTime.js
+++ www/js/components/shortTime/shortTime.js
@@ -6,10 +6,12 @@
   function shortTimeFunc (BaseComponent, TickService) {
     var shortTime = new BaseComponent();
 
-    shortTime.template = '<div class="short-time">{{now | moment: \'h:mm\'}}</div>';
+    shortTime.template = '<div class="short-time" editable-component>{{now | moment: \'h:mm\'}}</div>';
     shortTime.controller = shortTimeController;
 
     function shortTimeController ($scope, $element, $attrs) {
+      $scope.componentId = $attrs.componentId;
+      
       TickService.registerJob(_tick, true);
 
       function _tick () {
