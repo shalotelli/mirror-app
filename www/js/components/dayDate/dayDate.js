03397ca6c21c0ba23524c9b5d478b61fc40df318
Sha <sha@responsiveshops.com>
2015-09-15 01:59:07 -0400

    update dayDate component

--- www/js/components/dayDate/dayDate.js
+++ www/js/components/dayDate/dayDate.js
@@ -6,10 +6,12 @@
   function dayDateFunc (BaseComponent, TickService) {
     var dayDate = new BaseComponent();
 
-    dayDate.template = '<div class="day-date">{{now | moment: \'dddd [the] Mo\'}}</div>';
+    dayDate.template = '<div class="day-date" editable-component>{{now | moment: \'dddd [the] Do\'}}</div>';
     dayDate.controller = dayDateController;
 
     function dayDateController ($scope, $element, $attrs) {
+      $scope.componentId = $attrs.componentId;
+      
       TickService.registerJob(_tick, true);
 
       function _tick () {
