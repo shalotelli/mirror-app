6c9719aecf08ee33094303fb161b3a2c82c3e9c1
Sha <sha@responsiveshops.com>
2015-09-29 00:27:08 -0400

    server dashboard api

---
+++ server/api/controllers/DashboardController.js
@@ -0,0 +1,11 @@
+/**
+ * DashboardController
+ *
+ * @description :: Server-side logic for managing dashboards
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
